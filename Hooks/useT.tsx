45a3dabed459e4669dc0d2d6c08a9679de7bc928
mpoapostolis <mpoapostolis@gmail.com>
2023-02-22 11:21:27 +0200

    congrtz

--- Hooks/useT.tsx
+++ Hooks/useT.tsx
@@ -44,7 +44,11 @@ type Key =
   | "clock_pp5_author"
   | "error"
   | "password_does_not_match"
-  | "username_already_exists";
+  | "username_already_exists"
+  | "congrats"
+  | "congrats_text"
+  | "congrats_hint"
+  | "congrats_continue";
 
 const translations: Record<Locale, Record<Key, string>> = {
   en: {
@@ -121,6 +125,13 @@ to overcome the density of substance
 that then closed upon me,
 an obstacle visible to the light of heaven.`,
     clock_pp5_author: `Persephone, Zoe Karelli, 1973`,
+    congrats: `Congratulations`,
+    congrats_text: `
+    By successfully completing the game's "light-dark" track, you became a certified Initiate*. We hope you had a great time.
+    Soon you will have the opportunity to try the new sequel that is being prepared, as well as other surprises.
+    `,
+    congrats_continue: `Play again`,
+    congrats_hint: `* someone who  initiated into a religious teaching or ceremony accessible to a few chosen only`,
   },
   el: {
     menu_play: "Παίξε",
@@ -199,135 +210,13 @@ nl εμπόδιο φανερό στο φως τ’ ουρανού.
     error: `Το Ονομα χρήστη ή κωδικός πρόσβασης είναι εσφαλμένος`,
     password_does_not_match: `Οι κωδικοί πρέπει να είναι ίδιοι`,
     username_already_exists: `Το όνομα χρήστη υπάρχει ήδη`,
+
+    congrats: `Συγχαρητήρια`,
+    congrats_text: `Ολοκληρώνοντας με επιτυχία την πίστα «φως-σκοτάδι» του παιχνιδιού, είσαι πλέον και με τη «βούλα», ένας ΜΥΣΤΗΣ*. Ελπίζουμε να πέρασες καλά. Σύντομα θα έχεις τη δυνατότητα να δοκιμάσεις και την καινούργια πίστα που ετοιμάζεται, αλλά και άλλες εκπλήξεις.`,
+    congrats_continue: `Παίξε πάλι`,
+    congrats_hint: `* αυτός που μυήθηκε σε θρησκευτική διδασκαλία ή τελετή προσιτή σε λίγους`,
   },
 };
-
-// const _translations: Record<Locale, Record<Key, string>> = {
-//   en: {
-//     menu_play: "Play",
-//     menu_select_level: "Select Level",
-//     menu_achievements: "Achievements",
-//     menu_logout: "Logout",
-//     menu_reset: "Reset",
-//     login_title: `User info`,
-//     login_button: `Login`,
-//     login_signup: `Don't have an account yet? Sign up`,
-//     login_username: `Username`,
-//     login_password: `Password`,
-//     register_button: `Register`,
-//     register_title: `User info`,
-//     register_login: `Already have an account? Login`,
-//     register_username: `Username`,
-//     register_password: `Password`,
-//     register_password_confirm: `Confirm Password`,
-//     ready_new_game: `New Game`,
-//     ready_level: `Level`,
-//     ready_back: `Back`,
-//     ready_text: ``,
-//     ready_easy: `Easy`,
-//     ready_hard: `Hard`,
-//     ready_continue: `Continue`,
-//     ready_title: `Scenario`,
-//     ui_inventory: `Inventory`,
-//     ui_time: `Time Remaining`,
-//     ui_play_the_game: `Play the game`,
-//     clock_text: `Form 4 words related to the pair of opposites
-//                 "speech and silence". Only in this way will you find the secret that it is
-//                 imprisoned in time.`,
-//     reward_title: `Congratulations! You won the room's reward`,
-//     reward_continue: `Continue your journey`,
-//     wordsearch_title_cerberus: `Find words or phrases with the characteristics of the animal as mentioned in the text.`,
-//     wordsearch_text_cerberus: ``,
-//     wordsearch_author_cerberus: ``,
-//     wordsearch_title_pp4: `In the cryptword look for four phrases that oscillate between life and death, earth and sky, night and day…`,
-//     wordsearch_text_pp4: `
-//   The Starry Night
-//   The ship has crossed the glossy night bringing back Persephone. The joy is so great, even the stars have gone down low. But they remain untouched to maintain balance.
-//   The couple at the foot of the hill are the Maiden and the painter. He confides the secrets of his soul that drive him to death. The goddess, for her part, peacefully initiates him into Hades, while urging him to reconsider life.`,
-//     wordsearch_author_pp4: ``,
-//     learn_more: `Learn more`,
-//     clock_pp5_title: `Search through the ‘lengths’ and the ‘widths’ of the poem to find the words starting with the same letter to complete the ‘Compass of the Caryatid of Eleusis`,
-//     clock_pp5_text: `My meek struggle,
-// to cast off the tinge of shadow
-// from my face, knowledge
-// of the most secret expression, though I emerge
-// from the darkness.
-// Aphrodite, emerging from the waters
-// the generator of coiling, the transparent
-// and mighty beauty, help me
-// to overcome the density of substance
-// that then closed upon me,
-// an obstacle visible to the light of heaven.`,
-//     clock_pp5_author: `Persephone, Zoe Karelli, 1973`,
-//   },
-//   el: {
-//     menu_play: "Παίξε",
-//     menu_select_level: "Επιλογή Επιπέδου",
-//     menu_achievements: "Επιτεύγματα",
-//     menu_logout: "Αποσύνδεση",
-//     menu_reset: "Επαναφορά",
-//     login_title: `Στοιχεία χρήστη`,
-//     login_button: `Είσοδος`,
-//     login_username: `Όνομα χρήστη`,
-//     login_password: `Συνθηματικό`,
-//     login_signup: `Δεν έχετε λογαριασμό; Εγγραφείτε εδώ`,
-//     register_button: `Εγγραφή`,
-//     register_title: `Στοιχεία χρήστη`,
-//     register_login: `Έχετε ήδη λογαριασμό; Συνδεθείτε εδώ`,
-//     register_username: `Όνομα χρήστη`,
-//     register_password: `Συνθηματικό`,
-//     register_password_confirm: `Επιβεβαίωση Συνθηματικού`,
-//     ready_title: "Απόδραση στον Πολιτισμό” -  Ελευσίνα",
-//     ready_continue: "Συνέχεια",
-//     ready_new_game: "Νέο παιχνίδι",
-//     ready_level: "Επίπεδο",
-//     ready_back: "Πίσω",
-//     ready_text: `Όλα όσα πρόκειται να δεις από εδώ και πέρα δεν επιτρέπεται να τα πεις σε κανέναν!
-// 			Θα κινηθείς ανάμεσα στο Φως και το Σκοτάδι!
-// 			Θα χαθείς ανάμεσα στο Παρελθόν και το Παρόν!
-// 			Θα συρθείς ανάμεσα στο Υπόγειο και το Επίγειο!
-// 			Θα μπερδευτείς ανάμεσα στο Είναι και το Φαίνεσθαι!
-// 			Μην φοβηθείς και μην σταματήσεις! Θα ανταμειφθείς γι’ αυτό. Θα γίνεις κι εσύ ένας μύστης!
-// 			Διάβασε τα λογοτεχνικά κείμενα καθώς σε αυτά κρύβονται οι απαντήσεις σε όσα θα σου συμβούν. Στη διαδρομή, θα μαζέψεις αντικείμενα και θα λύσεις γρίφους για να καταφέρεις να διαφύγεις από κάθε χ			ώρο στον οποίο θα παγιδευτείς. Σε κάθε διαφυγή, θα συλλέγεις κι από ένα αντικείμενο, που θα σε βοηθήσει στην τελική σου απόδραση.
-// 			Καλή τύχη!`,
-//     ready_easy: "Εύκολο",
-//     ready_hard: "Δύσκολο",
-//     ui_inventory: "Η συλλογή μου",
-//     ui_time: "Χρόνος που απομένει",
-//     ui_play_the_game: "Παίξε το παιχνίδι",
-//     clock_text: `Σχημάτισε 4 λέξεις που σχετίζονται με το ζευγάρι των αντιθέσεων «ομιλία και σιωπή» για να βρεις το μυστικό που είναι φυλακισμένο στον χρόνο. Θα σε βοηθήσουν οι λέξεις που προβάλλονται στα "ρολόγια των λέξεων". `,
-//     reward_title: `Συγχαρητήρια! κέρδισες το έπαθλο του δωματίου.`,
-//     reward_continue: `Συνέχισε τη διαδρομή σου.`,
-//     wordsearch_title_cerberus: `Βρες λέξεις ή φράσεις με τα χαρακτηριστικά του ζώου όπως αναφέρονται στο κείμενο.`,
-//     wordsearch_text_cerberus: `Κει πέρα τίποτα δεν ταράζει τη σιωπή.Μονάχα ένας σκύλος (κι αυτός
-//                nl δε γαβγίζει), άσκημος σκύλος, ο δικός του, σκοτεινός με στραβά
-//                nl δόντια, με δυο μεγάλα μάτια αόριστα, πιστά και ξένα, σκοτεινά σαν
-//                nl πηγάδια, — κι ούτε ξεχωρίζεις μέσα τους το πρόσωπό σου, τα χέρια
-//                nl σου ή το πρόσωπό του.Ωστόσο διακρίνεις το σκοτάδι ακέριο, συμπαγές
-//                nl και διάφανο, πλήρες, παρηγορητικό, αναμάρτητο.Περσεφόνη,`,
-//     wordsearch_author_cerberus: `Γ.Ρίτσος`,
-//     wordsearch_title_pp4: `Αναζήτησε στο κρυπτόλεξο τις τέσσερις φράσεις που ταλαντεύονται μεταξύ ζωής και θανάτου, γης και ουρανού, νύχτας και μέρας…`,
-//     wordsearch_text_pp4: `Η έναστρη νύχτα
-//   Το πλοίο έχει διασχίσει το στιλπνό βράδυ φέρνοντας πίσω την Περσεφόνη. Η χαρά τόση, ακόμα και τ’ αστέρια έχουν κατεβεί χαμηλά. Αλλά παραμένουν ανέγγιχτα για διατήρηση της ισορροπίας.
-//   Το ζευγάρι στους πρόποδες του υψώματος είναι η Κόρη και ο ζωγράφος. Εκείνος εμπιστεύεται τα μυστικά της ψυχής του που τον ωθούν στο θάνατο. Η θεά από τη μεριά της τον μυεί γαλήνια στον Άδη, ενώ ταυτόχρονα προτρέπει για επανεξέταση της ζωής.`,
-//     wordsearch_author_pp4: ``,
-//     learn_more: `Μάθε περισσότερα`,
-//     clock_pp5_title: `Ψάξε σε όλα τα ‘μήκη’ και τα ‘πλάτη’ του ποιήματος να βρεις τις λέξεις που ξεκινούν από το ίδιο γράμμα και συμπλήρώσε την Πυξίδα της Καρυάτιδας της Ελευσίνας`,
-//     clock_pp5_text: `Δεινή μου πάλη,
-// nl για ν’ αποβάλλω τη χροιά της σκιάς
-// nl απ’ το πρόσωπό μου, γνώση
-// nl της πιο μυστικής έκφρασης κι ας αναδύομαι
-// nl απ’ το σκοτάδι.
-// nl Κύπρις, αναδυομένη απ’ των νερών
-// nl τη γεννήτρα συσπείρωση, τη διάφανη
-// nl και παντοδύναμη καλλονή, βοήθησέ με
-// nl την πυκνότητα να ξεπεράσω της ύλης
-// nl που, τότε, κλείστηκε απάνω μου,
-// nl εμπόδιο φανερό στο φως τ’ ουρανού.
-// `,
-//     clock_pp5_author: `Περσεφόνη, Ζωή Καρέλλη, 1973`,
-//   },
-// };
 
 export function useT() {
   const router = useRouter();
--- pages/congrats.tsx
+++ pages/congrats.tsx
@@ -1,20 +1,61 @@
+import { GetServerSideProps } from "next";
 import Link from "next/link";
 import { useEffect } from "react";
+import { useT } from "../Hooks/useT";
+import { withSessionSsr } from "../lib/withSession";
 import { useStore } from "../store";
+import myDb from "../helpers/mongo";
+import { ObjectId } from "mongodb";
+import { User } from "../lib/users";
 
-export default function GameOver() {
+export default function GameOver(props: User) {
   const store = useStore();
+  const t = useT();
   return (
-    <main className="h-screen w-full flex flex-col justify-center items-center bg-black">
-      <h1 className="text-6xl font-extrabold text-white tracking-widest">
-        Συγχαρητήρια 🎉🎉🎉🎉🎉🎉🎉
-      </h1>
-      <br />
-      <form action="/api/auth?type=reset" method="POST">
-        <a className="mt-5 w-full">
-          <button className="btn w-72   btn-xl">Παίξε πάλι</button>
-        </a>
-      </form>
+    <main
+      style={{
+        backgroundImage: `url(/images/congrats.jpg) `,
+      }}
+      className="h-screen bg-auto bg-no-repeat bg-center  w-full flex flex-col justify-center  items-center bg-black"
+    >
+      <div className="max-w-4xl text-center mx-auto  bg-black bg-opacity-50 p-8">
+        <h1 className="text-6xl font-extrabold text-white">
+          {t("congrats")} {props.userName}!
+        </h1>
+        <div className="divider"></div>
+        <div className="mx-auto felx   max-w-xl">
+          <p className="text-lg text-white font-bold ">
+            {t("congrats_text")}
+            <br />
+          </p>
+          <span className="text-sm block mt-10">{t("congrats_hint")}</span>
+          <br />
+          <br />
+        </div>
+        <form action="/api/auth?type=reset" method="POST">
+          <a className="mt-5 w-full">
+            <button className=" hover:scale-105 w-full bg-black px-8 py-4 rounded-xl border border-dashed border-white  text-center bg-opacity-70 text-xl font-bold text-white drop-shadow ">
+              {t("congrats_continue")}
+            </button>
+          </a>
+        </form>
+      </div>
     </main>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = withSessionSsr(
+  async function getServerSideProps({ req }) {
+    const user = req.session.user;
+    let destination = null;
+    const db = await myDb();
+
+    const account = await db
+      .collection("users")
+      .findOne({ _id: new ObjectId(`${user?.id ?? ""}`) });
+
+    return {
+      props: { userName: account?.userName },
+    };
+  }
+);
